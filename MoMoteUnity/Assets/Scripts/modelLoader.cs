3f5e12d28980ce442426dcedff6c3f7935c48564
Compaagnie <57749949+Compaagnie@users.noreply.github.com>
2023-03-22 19:15:07 +0100

    start of scripts for file explorer and working menu

---
+++ MoMoteUnity/Assets/Scripts/modelLoader.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class modelLoader : MonoBehaviour
+{
+    public string modelName;
+    public string modelPath;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // set Model Name game object text
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void loadModel(){
+        // open scene with model from path
+    }
+}
