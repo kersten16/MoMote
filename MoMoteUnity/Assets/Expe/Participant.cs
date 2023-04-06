d5045ff69803bd293b245b1f4b5a9b27530a3c48
Kersten Smith <kerstenmds@gmail.com>
2023-04-06 21:51:34 +0200

    changed trial array to list

--- MoMoteUnity/Assets/Expe/Participant.cs
+++ MoMoteUnity/Assets/Expe/Participant.cs
@@ -5,9 +5,9 @@ using UnityEngine;
 public class Participant
 {
     public int ID { get; set; }
-    public Trial[] Trials { get; set; }
+    public List<Trial> Trials { get; set; }
 
-    public Participant (int ID,Trial[] Trials) {
+    public Participant (int ID,List<Trial> Trials) {
         this.ID = ID;
         this.Trials = Trials;
     }
